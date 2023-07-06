6fc34691aee20a0da5d5d0857dfd9e99e05c1a2f
EdwardArvinius <edward.arvinius@appliedtechnology.se>
2023-07-06 14:45:59 +0200

    created new file Pokedex.tsx

--- server/src/main/java/com/pokemon/pokemon/controller/PokemonController.java
+++ server/src/main/java/com/pokemon/pokemon/controller/PokemonController.java
@@ -1,19 +1,12 @@
 package com.pokemon.pokemon.controller;
 
 
-
-
-
 import com.pokemon.pokemon.model.Pokemon;
 import com.pokemon.pokemon.service.PokemonService;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 @CrossOrigin(origins = "http://127.0.0.1:5173")
 @RestController
