7113bcc6cd2ce24136bea65025d03621884959b1
EdwardArvinius <edward.arvinius@appliedtechnology.se>
2023-07-06 11:32:21 +0200

    restarted on PokemonSearch Component

--- client/src/components/PokemonSearch.tsx
+++ client/src/components/PokemonSearch.tsx
@@ -1,3 +1,14 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+
+interface Pokemon {
+name: string;
+abilities: string[];
+stats: Record<string, number>;
+type: string;
+image: string;
+}
 
 
 
