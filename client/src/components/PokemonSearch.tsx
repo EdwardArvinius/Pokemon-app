8cc39c32f32eb799ca5c004e559be59bfd0b782c
EdwardArvinius <edward.arvinius@appliedtechnology.se>
2023-07-06 11:44:20 +0200

    set up props and fetch in PokemonSearch component

--- client/src/components/PokemonSearch.tsx
+++ client/src/components/PokemonSearch.tsx
@@ -10,5 +10,35 @@ type: string;
 image: string;
 }
 
+const PokemonSearch: React.FC = () => {
+    const [pokemonName, setPokemonName] = useState<string>('');
+    const [pokemonData, setPokemonData] = useState<Pokemon | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
+    
+    
+    const searchPokemon = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+    const res = await axios.get<Pokemon>(`http://localhost:8080/api/v2/pokemon/${pokemonName}`);
+    setPokemonData(res.data);
+    } catch (err) {
+    setError(err.message);
+    }
+    setIsLoading(false);
+    };
+    
+    
+    return (
+    <div>
+    
+    </div>
+    );
+    };
+    
+    
+    export default PokemonSearch;
+    
 
 
